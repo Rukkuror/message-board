959546a453d25f81d982c2e49700138c65f4c1e2
Rukmangathan Govindarajan <mangathan.ruk@gmail.com>
2017-05-28 09:06:17 +0530

    Change in font url

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module MessageBoard
   class Application < Rails::Application
-  	config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+  	config.assets.paths << "#{Rails.root}/app/assets/fonts"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
