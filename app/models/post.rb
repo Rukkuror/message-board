c565685119419ea48f86582e022226769ba74227
Rukmangathan Govindarajan <mangathan.ruk@gmail.com>
2017-05-29 16:31:03 +0530

    change in scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,8 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   acts_as_votable
 
-  scope :search_all, ->(params) { where("(lower(title) LIKE ? or lower(description) LIKE ?) AND (user_id = ?)", "%#{params[:search]}%", "%#{params[:search]}%", params[:user_id])}
-  scope :search_post, ->(params) { where("lower(title) LIKE ? or lower(description) LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")}
+  scope :search_all, ->(params) { where("(title ILIKE ? or description ILIKE ?) AND (user_id = ?)", "%#{params[:search]}%", "%#{params[:search]}%", params[:user_id])}
+  scope :search_post, ->(params) { where("title ILIKE ? or description ILIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")}
   scope :search_user, ->(params) { where("user_id = ?", params[:user_id])}
   
   def editable?
