b361c96e34893cf88088194904b8272d8faf8e62
Rukmangathan Govindarajan <mangathan.ruk@gmail.com>
2017-05-29 08:54:35 +0530

    Changes in post index action

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ApplicationRecord
 
   scope :search_all, ->(params) { where("(title LIKE ? or description LIKE ?) AND (user_id = ?)", "%#{params[:search]}%", "%#{params[:search]}%", params[:user_id])}
   scope :search_post, ->(params) { where("title LIKE ? or description LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")}
-  scope :search_user, ->(params) { where("user_id LIKE ?", params[:user_id])}
+  scope :search_user, ->(params) { where("user_id = ?", params[:user_id])}
   
   def editable?
     self.created_at > (Time.now - 1.hour)
