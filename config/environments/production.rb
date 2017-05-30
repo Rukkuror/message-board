af313c864322e46907a779b881e30d0c7caad199
Rukmangathan Govindarajan <mangathan.ruk@gmail.com>
2017-05-30 23:38:08 +0530

    Change in environment file for assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   config.assets.compile = true
 
   # For static assets
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
